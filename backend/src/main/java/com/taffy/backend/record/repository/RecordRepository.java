a5569e30f6d06b29b72efc99f38db74e1ac37fa9
차봉석 <chaqhdtjr7@gmail.com>
2024-07-22 01:47:59 +0900

    [BE] [fix] #4 : RecordRepository import 오류 수정

--- backend/src/main/java/com/taffy/backend/record/repository/RecordRepository.java
+++ backend/src/main/java/com/taffy/backend/record/repository/RecordRepository.java
@@ -1,5 +1,6 @@
 package com.taffy.backend.record.repository;
 
+import com.taffy.backend.record.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
