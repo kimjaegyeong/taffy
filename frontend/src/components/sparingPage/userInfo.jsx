13c8000ec1732f408685bf34efa7be7443984ff1
진아현 <jah000118@naver.com>
2024-07-24 14:42:29 +0900

    [FE] [design] #10 : 유저정보창 디자인 수정

--- frontend/src/components/sparingPage/userInfo.jsx
+++ frontend/src/components/sparingPage/userInfo.jsx
@@ -3,15 +3,15 @@ import '../../styles/sparingPage/userInfo.css'
 const userInfo = () => {
   return (
   <div className="box">
-    <div className="view">
-      <div className="overlap-group">
-        <img className="vector" alt="Vector" src="..\src\assets\images\sparingPage\main-userinfo-vector.png" />
-        <div className="userInfoBox">
-          <div className="nickname">진라면</div>
-          <div className="div">빨간띠</div>
+    <div className="overlap-group">
+      <div className="userInfoBox">
+        <div className="nickname">banana</div>
+        <div className="beltInfo">
+          <div className="beltname">red</div>
           <img className="belt" alt="belt" src="..\src\assets\images\common\belt\redBelt.png" />
         </div>
       </div>
+      <img className="vector" alt="Vector" src="..\src\assets\images\sparingPage\main-userinfo-vector.png" />
     </div>
   </div>
   )
