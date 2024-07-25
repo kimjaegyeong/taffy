adeda0ae578a375ffb9455d7d173477ae97563bd
문자영 <mremi99@naver.com>
2024-07-25 16:09:52 +0900

    [FE] [feat] #13 : navbar 한영 전환

--- frontend/src/components/common/navbar.jsx
+++ frontend/src/components/common/navbar.jsx
@@ -2,14 +2,17 @@ import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../../styles/common/navbar.css';
 import logo from '../../assets/images/common/logo.png';
-import loginEn from '../../assets/images/common/Login.png';
-import logoutEn from '../../assets/images/common/Logout.png';
+import loginEn from '../../assets/images/common/login.png';
+import logoutEn from '../../assets/images/common/logout.png';
 import myEn from '../../assets/images/common/My page.png';
 import signupEn from '../../assets/images/common/Sign up.png';
 import loginKo from '../../assets/images/common/로그인.png';
 import logoutKo from '../../assets/images/common/로그아웃.png';
 import myKo from '../../assets/images/common/마이페이지.png';
 import signupKo from '../../assets/images/common/회원가입.png';
+import Language from '../../assets/images/common/Language.png';
+import LanguageKo from '../../assets/images/common/언어설정.png';
+import Dropdown from './dropDown';
 
 const Navbar = ({ isLoggedIn, handleLogin, handleLogout, language, setLanguage }) => {
   const location = useLocation();
@@ -22,6 +25,7 @@ const Navbar = ({ isLoggedIn, handleLogin, handleLogout, language, setLanguage }
         logout: logoutKo,
         my: myKo,
         signup: signupKo,
+        language: LanguageKo
       };
     }
     return {
@@ -29,10 +33,15 @@ const Navbar = ({ isLoggedIn, handleLogin, handleLogout, language, setLanguage }
       logout: logoutEn,
       my: myEn,
       signup: signupEn,
+      language: Language
     };
   };
 
-  const { login, logout, my, signup } = getLanguageImages();
+  const { login, logout, my, signup} = getLanguageImages();
+
+  const languageOptions = [
+    { value: 'ko', label: '한국어' },
+    { value: 'en', label: 'English' }]
 
   return (
     <nav className="navbar">
@@ -43,26 +52,25 @@ const Navbar = ({ isLoggedIn, handleLogin, handleLogout, language, setLanguage }
       </div>
       {!isMainOrLanding && (
         <div className="links">
-          <Link to="/ps_edu">{language === 'ko' ? '품새 교육' : 'Poomsae Education'}</Link>
+          <Link to="/ps_edu">{language === 'ko' ? '품새 교육' : 'Poomsae Edu'}</Link>
           <Link to="/ps_test">{language === 'ko' ? '품새 심사' : 'Poomsae Test'}</Link>
-          <Link to="/sp">{language === 'ko' ? '겨루기' : 'Sparring'}</Link>
+          <Link to="/sp">{language === 'ko' ? '겨루기' : 'Gyeorugi'}</Link>
         </div>
       )}
       <div className="icons">
+        <Dropdown
+          options={languageOptions}
+          selectedOption={language === 'ko' ? LanguageKo : Language}
+          onOptionSelect={(option) => setLanguage(option)}
+        />
         {isLoggedIn ? (
           <>
             <a onClick={handleLogout}>
               <img src={logout} alt="logout" className="iconImage" />
             </a>
-            <div className="dropdown">
-              <img src={my} alt="my" className="myIcon" />
-              <div className="dropdownContent">
-                <Link to="/my">{language === 'ko' ? '마이 페이지' : 'My Page'}</Link>
-                <a onClick={() => setLanguage(language === 'ko' ? 'en' : 'ko')}>
-                  {language === 'ko' ? 'English' : '한국어'}
-                </a>
-              </div>
-            </div>
+            <Link to="/my">
+              <img src={my} alt="mypage" className="iconImage" />
+            </Link>
           </>
         ) : (
           <>
