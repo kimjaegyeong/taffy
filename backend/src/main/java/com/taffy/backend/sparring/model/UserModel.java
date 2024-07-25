069b091c60ac43e814f6081ae6bf652a50a777c6
김재경 <dongji_11@naver.com>
2024-07-25 17:24:24 +0900

    Update backend/src/main/java/com/taffy/backend/sparring/model/UserModel.java

--- backend/src/main/java/com/taffy/backend/sparring/model/UserModel.java
+++ backend/src/main/java/com/taffy/backend/sparring/model/UserModel.java
@@ -24,4 +24,19 @@ public class UserModel implements Serializable {
     private int winCnt;
     private int loseCnt;
     private int drawCnt;
+
+       @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return userId == userModel.userId && winCnt == userModel.winCnt && loseCnt == userModel.loseCnt && drawCnt == userModel.drawCnt && Objects.equals(nickname, userModel.nickname) && Objects.equals(beltName, userModel.beltName) && Objects.equals(profileImg, userModel.profileImg);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, nickname, beltName, profileImg, winCnt, loseCnt, drawCnt);
+    }
 }
+
+
