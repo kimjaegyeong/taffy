3db2748de8895e04f9083dec324bad1c0a550df8
차봉석 <chaqhdtjr7@gmail.com>
2024-07-28 18:10:02 +0900

    [BE] [feat] #14 Belt, Member Entity 수정 PK AI 설정 Member Entity 일대일에서 다대일로 변경

--- backend/src/main/java/com/taffy/backend/member/domain/Member.java
+++ backend/src/main/java/com/taffy/backend/member/domain/Member.java
@@ -33,7 +33,7 @@ public class Member extends BaseTime {
     @Column(name = "profile_img")
     private String profile_img;
 
-    @OneToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "belt_id")
     private Belt belt;
 
