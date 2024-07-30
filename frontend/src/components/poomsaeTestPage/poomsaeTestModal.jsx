6202e284ce14b3f66316f01750673dc0da0cd42f
강수연 <kangtea9@naver.com>
2024-07-30 16:59:15 +0900

    [FE] [style] #11 : 품새 선택 모달 레이아웃 및 디자인 변경

--- frontend/src/components/poomsaeTestPage/poomsaeTestModal.jsx
+++ frontend/src/components/poomsaeTestPage/poomsaeTestModal.jsx
@@ -20,12 +20,14 @@ const PoomsaeTestModal = ({ onClose }) => {
             </div>
             <div className="modal-desc">
               <div className="modal-ps-description">
-                <p>태극 1장은 팔괘 중에서 하늘을 상징하는 건(建)에 해당한다.건이란 태극에 있어 음과 양의 상대 작용에서 양(陽)을 가리키며,만물의 시작을 알리는 기운을 나타낸다.또한, 건은 인체에 있어서 머리에 해당하므로 첫 번째 품새 수련을 통해서 태권도의 기본이 되는 동작의 바른 이해와 기술 습득을 의미한다.</p>
+                <p>태극 1장은 팔괘 중에서 하늘을 상징하는 건(建)에 해당한다. 건이란 태극에 있어 음과 양의 상대 작용에서 양(陽)을 가리키며, 만물의 시작을 알리는 기운을 나타낸다. 또한, 건은 인체에 있어서 머리에 해당하므로 첫 번째 품새 수련을 통해서 태권도의 기본이 되는 동작의 바른 이해와 기술 습득을 의미한다.</p>
               </div>
-              <button className="modal-btn" onClick={handleChallengeClick}>도전하기</button>
+              <div className="modal-btn">
+                <button onClick={handleChallengeClick}>도전하기</button>
+              </div>
             </div>
           </div>
-          <button className="modal-close" onClick={onClose}>Close</button>
+          <button className="modal-close" onClick={onClose}>X</button>
         </div>
       </div>
     );
