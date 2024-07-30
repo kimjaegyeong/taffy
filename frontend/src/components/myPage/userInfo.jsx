b27e2776e8dd8f72aa87a8f79b242e79a149d9d6
진아현 <jah000118@naver.com>
2024-07-30 17:45:33 +0900

    [FE] [feat] #18 : 메인페이지 유저부분 디자인 토대 설정

--- frontend/src/components/myPage/userInfo.jsx
+++ frontend/src/components/myPage/userInfo.jsx
@@ -3,8 +3,20 @@ import '../../styles/myPage/userInfo.css'
 const UseInfo = () => {
   return (
     <div className="userinfobox">
-      
-      <p>2</p>
+      <div className="characterphoto">
+        <p>사진</p>
+      </div>
+      <div className="characterinfomation">
+        <p>닉네임</p>
+        <p>띠이름</p>
+        <p>띠사진</p>
+      </div>
+      <div className="poomsaeeducation">
+        <p>연습 진행사항</p>
+      </div>
+      <div className="winpoint">
+        <p>승률</p>
+      </div>
       <button>수정</button>
     </div>
   )
