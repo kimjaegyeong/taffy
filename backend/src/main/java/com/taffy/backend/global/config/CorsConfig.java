7e3878395cca5bccbefe2bf795b123e6ce8eaaea
김재경 <dongji_11@naver.com>
2024-08-01 12:14:34 +0900

    [BE][fix] error 발생 CORS 제외

--- backend/src/main/java/com/taffy/backend/global/config/CorsConfig.java
+++ backend/src/main/java/com/taffy/backend/global/config/CorsConfig.java
@@ -1,27 +1,27 @@
-package com.taffy.backend.global.config;
+// package com.taffy.backend.global.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
+//     @Bean
+//     public CorsFilter corsFilter() {
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        //config.addAllowedOriginPattern("https://i11e104.p.ssafy.io/api/**");
-        config.addAllowedHeader("*");
-        //config.addExposedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-}
+//         config.setAllowCredentials(true);
+//         config.addAllowedOriginPattern("*");
+//         //config.addAllowedOriginPattern("https://i11e104.p.ssafy.io/api/**");
+//         config.addAllowedHeader("*");
+//         //config.addExposedHeader("*");
+//         config.addAllowedMethod("*");
+//         source.registerCorsConfiguration("/**", config);
+//         return new CorsFilter(source);
+//     }
+// }
