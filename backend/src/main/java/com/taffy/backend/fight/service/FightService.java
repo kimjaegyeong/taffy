50f008ebe16cb078764dae6ff9e2d85d54c7f4fb
김재경 <dongji_11@naver.com>
2024-08-03 19:26:00 +0900

    [BE][feat] #20 openvidu 를 통한 방만들기, 참여 구현

--- backend/src/main/java/com/taffy/backend/fight/service/FightService.java
+++ backend/src/main/java/com/taffy/backend/fight/service/FightService.java
@@ -1,15 +1,25 @@
 package com.taffy.backend.fight.service;
-
-import com.taffy.backend.fight.dto.InviteRoomRequestDto;
+import com.taffy.backend.fight.controller.FightController;
+import com.taffy.backend.fight.dto.ConnectionInfoDto;
+import io.openvidu.java.client.Connection;
+import io.openvidu.java.client.ConnectionProperties;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.taffy.backend.fight.dto.RedisHashUser;
 import com.taffy.backend.global.exception.ErrorCode;
 import com.taffy.backend.global.exception.TaffyException;
 import com.taffy.backend.member.domain.Member;
 import com.taffy.backend.member.repository.MemberRepository;
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.Cursor;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -17,27 +27,40 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class FightService {
-
+    private static final Logger log = LoggerFactory.getLogger(FightController.class);
     private final MemberRepository memberRepository;
     private final RedisTemplate<String, Object> redisTemplate;
 
-    private static final String INVITE_PREFIX = "invite:";
     private static final String ROOM_PREFIX = "taffy:";
     private final LocalContainerEntityManagerFactoryBean entityManagerFactory;
 
     @Transactional(readOnly = true)
-    public String createRoom(Long memberId, String sessionId) {
+    public ConnectionInfoDto createRoom(Long memberId, String sessionId)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        //member 존재여부 체크
+        RedisHashUser redisHashUser = createRedisHashUser(memberId);
+        //redis에 저장
+        redisTemplate.opsForList().rightPush(ROOM_PREFIX+sessionId, redisHashUser);
+        //openvidu에서 session 생성
+        initializeSession(memberId);
+        //openvidu 미디어서버로 connection
+        Map<String, Object> map =new HashMap<>();
+        map.put("sessionId", sessionId);
+        map.put("memberId", memberId);
+        String connectionToken = joinSession(map);
+        return  new ConnectionInfoDto(sessionId, connectionToken);
+    }
 
+    private RedisHashUser createRedisHashUser(Long memberId) {
+        // 해당 member의 존재여부 체크
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new TaffyException(ErrorCode.MEMBER_NOT_FOUND));
 
+        //redis에 객체를 저장할 수 있도록 변환
         RedisHashUser redisHashUser = RedisHashUser.builder()
                 .id(member.getId())
                 .email(member.getEmail())
@@ -47,38 +70,13 @@ public class FightService {
                 .nickName(member.getNickname())
                 .beltName(member.getBelt().getBelt_name())
                 .build();
-
-//        String roomId = "room:" + member.getNickname()+":"+System.currentTimeMillis();
-        redisTemplate.opsForList().rightPush(ROOM_PREFIX+sessionId, redisHashUser);
-        return sessionId;
-    }
-
-    @Transactional(readOnly = true)
-    public void invite(InviteRoomRequestDto inviteRoomRequestDto) {
-        Member member = memberRepository.findByNickname(inviteRoomRequestDto.getNickName()).orElseThrow(() -> new TaffyException(ErrorCode.MEMBER_NOT_FOUND));
-        String inviteKey = INVITE_PREFIX + member.getNickname();
-        redisTemplate.opsForValue().set(inviteKey, inviteRoomRequestDto.getRoomId(), 3, TimeUnit.MINUTES);
+        return redisHashUser;
     }
 
     @Transactional(readOnly = true)
     public void enterRoom(Long memberId, String roomId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(() -> new TaffyException(ErrorCode.MEMBER_NOT_FOUND));
+        RedisHashUser redisHashUser = createRedisHashUser(memberId);
 
-        RedisHashUser redisHashUser = RedisHashUser.builder()
-                .id(member.getId())
-                .email(member.getEmail())
-                .win(member.getRecord().getWin())
-                .draw(member.getRecord().getDraw())
-                .loss(member.getRecord().getLose())
-                .nickName(member.getNickname())
-                .beltName(member.getBelt().getBelt_name())
-                .build();
-
-//        if (roomId != null && redisTemplate.opsForList().size(roomId) < 2) {
-//            redisTemplate.opsForList().rightPush(roomId, redisHashUser);
-//            redisTemplate.delete(INVITE_PREFIX + member.getNickname());
-//            return;
-//        }
         if(roomId != null && redisTemplate.opsForList().size(roomId) < 2 ){
             redisTemplate.opsForList().rightPush(roomId, redisHashUser);
             System.out.println("방 입장 완료 :" + roomId );
@@ -87,37 +85,84 @@ public class FightService {
 
         throw new TaffyException(ErrorCode.CANNOT_JOIN_ROOM);
     }
+    @Transactional(readOnly = true)
+    public ConnectionInfoDto joinRoom(Long memberId, String sessionId) throws OpenViduJavaClientException, OpenViduHttpException {
+        RedisHashUser redisHashUser = createRedisHashUser(memberId);
+        redisTemplate.opsForList().rightPush(sessionId, redisHashUser);
+        Map<String, Object> map =new HashMap<>();
+        map.put("sessionId", sessionId);
+        map.put("memberId", memberId);
+        String connectionToken = joinSession(map);
+        return new ConnectionInfoDto(sessionId, connectionToken);
+    }
 
-    public Map<String, Object> getAllKeys() {
-        Map<String, Object> keys = new HashMap<>();
+    @Transactional(readOnly = true)
+    public ConnectionInfoDto gameStart(Long memberId, String sessionId){
+        return null;
+    }
+
+    public String findAvailableRoom(){
         Cursor<byte[]> cursor = redisTemplate.getConnectionFactory().getConnection().scan(ScanOptions.scanOptions().match("*").build());
         while (cursor.hasNext()) {
             String key = new String(cursor.next());
-            Object value = redisTemplate.opsForList().range(key, 0, -1);
-
-//            HashOperations<String, String, Object> hashOps = redisTemplate.opsForHash();
-//            Map<String, Object> entries = hashOps.entries(key);
-
-//            if (entries.isEmpty()) {
-//                return null;
-//            }
-//
-//            RedisHashUser user = new RedisHashUser();
-//            user.setId(Long.valueOf((String) entries.get("id")));
-//            user.setEmail((String) entries.get("email"));
-//            user.setLoss(Integer.parseInt((String) entries.get("lose")));
-//            user.setWin(Integer.parseInt((String) entries.get("win")));
-//            user.setDraw(Integer.parseInt((String)entries.get("draw")));
-//            user.setNickName((String)entries.get("nickName"));
-//            user.setBeltName((String)entries.get("beltName"));
-
+            List<Object> value = redisTemplate.opsForList().range(key, 0, -1);
+            if (value != null && value.size() == 1) {
+                return key;
+            }
+        }
+        return "notfound";
+    }
+    public Map<String, Object> getOnePeopleRome() {
+        Map<String, Object> keys = new HashMap<>();
+        Map<String, Object> filteredKeys = new HashMap<>();
+        Cursor<byte[]> cursor = redisTemplate.getConnectionFactory().getConnection().scan(ScanOptions.scanOptions().match("*").build());
+        while (cursor.hasNext()) {
+            String key = new String(cursor.next());
+            List<Object> value = redisTemplate.opsForList().range(key, 0, -1);
+            if (value != null && value.size() == 1) {
+                filteredKeys.put(key, value);
+            }
             keys.put(key, value);
         }
-        return keys;
+        return filteredKeys;
     }
-
+    @Transactional(readOnly = true)
     public void getSpecificKey(String key) {
         Object value = redisTemplate.opsForList().range(key,0,-1);
         System.out.println("Key: " + key + ", Value: " + value);
     }
+
+    private OpenVidu openvidu;
+    @Value("${openvidu.url}")
+    private String OPENVIDU_URL;
+
+    @Value("${openvidu.secret}")
+    private String OPENVIDU_SECRET;
+
+
+    @PostConstruct
+    public void init() {
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+
+    public String initializeSession(Long memberId)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.createSession();
+        log.info(session.getSessionId());
+        return session.getSessionId();
+    }
+
+    // Map<String, String > params : sessionId, userName
+    public String joinSession(Map<String,Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(params.get("sessionId").toString());
+        if (session == null) {
+            new TaffyException(ErrorCode.CANNOT_JOIN_ROOM);
+        }
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        //frontend에서 openvidu 의 session에 접속하기 위해 반드시 필요한 것
+        return connection.getToken();
+    }
+
 }
