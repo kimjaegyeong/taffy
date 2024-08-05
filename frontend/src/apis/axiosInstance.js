aa91153d6b80f9ea40ebeb5f11b0ba0c754e2767
문자영 <mremi99@naver.com>
2024-08-05 09:49:12 +0900

    [FE] [feat] #16 : 동작 컴포넌트 내 번호 추가

--- frontend/src/apis/axiosInstance.js
+++ frontend/src/apis/axiosInstance.js
@@ -1,4 +1,3 @@
-// axiosInstance.js
 import axios from 'axios';
 
 const API_BASE_URL = 'https://i11e104.p.ssafy.io/api/';
@@ -10,7 +9,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // const token = localStorage.getItem('token'); // 토큰을 로컬 스토리지에서 가져온다고 가정
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJtZW1iZXJJZFwiOjh9IiwiaWF0IjoxNzIyNjgwMTM1LCJleHAiOjE3MjI2ODExMzV9.ENqx_iCAthFRcSEW0VkKoNI5086mPWZCEZxi7W1WD3I';
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJtZW1iZXJJZFwiOjh9IiwiaWF0IjoxNzIyODE3Mzc0LCJleHAiOjE3MjI4MTgzNzR9.KY9B3WT_orsC036OT-2BkQWcjBuMJ5AkpZNzA2Ab1gY';
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
