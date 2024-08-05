01768f2ea313ae3eaec27ba9728ed245d8244cff
차봉석 <chaqhdtjr7@gmail.com>
2024-08-05 17:42:07 +0900

    [BE][fix] #2 유저닉네임 범위 4~10자로 변경

--- backend/src/main/java/com/taffy/backend/member/dto/SignUpRequestDto.java
+++ backend/src/main/java/com/taffy/backend/member/dto/SignUpRequestDto.java
@@ -7,18 +7,18 @@ import lombok.Getter;
 public class SignUpRequestDto {
 
     @Email
-    @NotEmpty
+    @NotEmpty(message = "이메일은 필수 입력 항목입니다")
     private String email;
 
-    @NotEmpty
-    @Size(min = 4, max = 20)
+    @NotEmpty(message = "닉네임은 필수 입력 항목입니다")
+    @Size(min = 4, max = 10)
     private String nickName;
 
-    @NotEmpty
+    @NotEmpty(message = "비밀번호는 필수 입력 항목입니다")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$", message = "영문/숫자/특수문자 8~20자 이내로 작성해주세요")
     private String password;
 
-    @NotEmpty
+    @NotEmpty(message = "국가명은 필수 입력 항목입니다")
     private String countryName;
 
 }
