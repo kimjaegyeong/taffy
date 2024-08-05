6ad8a6c33d7583f684130dbb2c18a8eeadb8b49b
문자영 <mremi99@naver.com>
2024-08-05 09:55:09 +0900

    [FE] [remove] #16 : reducers, actions  파일 삭제

--- frontend/src/actions/actions.jsx
+++
@@ -1,7 +0,0 @@
-// export const SET_ACTIVE_ITEM = 'SET_ACTIVE_ITEM';
-// // export const SET_ACTIVE_STAGE = 'SET_ACTIVE_STAGE';
-
-// export const setActiveItem = (item) => ({
-//   type: SET_ACTIVE_ITEM,
-//   payload: item,
-// });
--- frontend/src/apis/axiosInstance.js
+++ frontend/src/apis/axiosInstance.js
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // const token = localStorage.getItem('token'); // 토큰을 로컬 스토리지에서 가져온다고 가정
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJtZW1iZXJJZFwiOjh9IiwiaWF0IjoxNzIyODE3Mzc0LCJleHAiOjE3MjI4MTgzNzR9.KY9B3WT_orsC036OT-2BkQWcjBuMJ5AkpZNzA2Ab1gY';
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJtZW1iZXJJZFwiOjh9IiwiaWF0IjoxNzIyODE5MTUzLCJleHAiOjE3MjI4MjAxNTN9.DbDBGlfuxXphqO1UVGCHCY6b3jQW5Ej8_qbqhX3uSHc';
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
