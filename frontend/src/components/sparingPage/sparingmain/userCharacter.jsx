3672a37745dea14cc8f81bec5627d701cad71c7c
진아현 <jah01894@gmail.com>
2024-08-05 17:36:58 +0900

    [FE] [feat] #25 : 겨루기 메인페이지 회원 정보 연동 성공

--- frontend/src/components/sparingPage/sparingmain/userCharacter.jsx
+++ frontend/src/components/sparingPage/sparingmain/userCharacter.jsx
@@ -1,8 +1,24 @@
 import '../../../styles/sparingPage/sparingmain/userCharacter.css'
 
-const userCharacter = () => {
+import Tiger from '../../../assets/images/sparingPage/tiger/호랑이 기본동작 1.png'
+import Bear from '../../../assets/images/sparingPage/bear/곰 기본동작 1.png'
+import Dragon from '../../../assets/images/sparingPage/dragon/용 기본동작 1.png'
+
+const userCharacter = ({profile}) => {
+  const getCharacterImgSrc = (charactername) => {
+    switch (charactername) {
+      case 'Tiger':
+        return Tiger;
+      case 'Bear':
+        return Bear;
+      case 'Dragon':
+        return Dragon;
+    }
+  }
+
   return (
     <div className="characterbox">
+      <img className="charactersparingimg" src={getCharacterImgSrc(profile.profileImg)} alt="" />
     </div>
   )
 }
--- frontend/src/components/sparingPage/sparingmain/userRecord.jsx
+++ frontend/src/components/sparingPage/sparingmain/userRecord.jsx
@@ -3,7 +3,10 @@ import SparInfoBottom from '../../../assets/images/sparingPage/spar-info-bottom.
 import SparInfoTop from '../../../assets/images/sparingPage/spar-info-top.png'
 
 
-const userRecord = () => {
+const userRecord = ({record}) => {
+  const winRate = (record.data.win + record.data.lose + record.data.draw) === 0 
+  ? 0 
+  : (record.data.win / (record.data.win + record.data.lose + record.data.draw)) * 100;
   return (
     <div className="recordbox">
       <div>
@@ -14,18 +17,18 @@ const userRecord = () => {
           <div className="totalcount">
             <p className="custom-title">총 횟수</p>
             <hr className="custom-hr"/>
-            <p style={{fontSize: '50px', margin: '0px'}}>20<span style={{fontSize: '20px'}}>회</span></p>
+            <p style={{fontSize: '50px', margin: '0px'}}>{record.data.win+record.data.lose+record.data.draw}<span style={{fontSize: '20px'}}>회</span></p>
           </div>
           <div className="wincount">
             <p className="custom-title">이긴 횟수</p>
             <hr className="custom-hr"/>
-            <p style={{fontSize: '50px', margin: '0px'}}>10<span style={{fontSize: '20px'}}>회</span></p>
+            <p style={{fontSize: '50px', margin: '0px'}}>{record.data.win}<span style={{fontSize: '20px'}}>회</span></p>
           </div>
         </div>
         <div className="winscore">
           <p className="custom-title">승률</p>
           <hr className="custom-hr"/>
-          <p style={{fontSize: '50px', margin: '0px'}}>50<span style={{fontSize: '20px'}}>%</span></p>
+          <p style={{fontSize: '50px', margin: '0px'}}>{isNaN(winRate) ? 0 : winRate.toFixed(1)}<span style={{fontSize: '20px'}}>%</span></p>
           </div>
       </div>
       <div style={{margin: '-4px', width: '99%'}}>
