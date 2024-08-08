50f76b36a2fb54a7f6154c66b742c12e60623aef
강수연 <kangtea9@naver.com>
2024-08-08 23:14:14 +0900

    [FE] [style] #11 : 품새별 이미지 경로 설정 변경

--- frontend/src/components/poomsaeTestPage/poomsaeTestModal.jsx
+++ frontend/src/components/poomsaeTestPage/poomsaeTestModal.jsx
@@ -1,6 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import '../../styles/poomsaeTestPage/poomsaeTestModal.css';
 import PropTypes from 'prop-types';
+import Ps1 from '/src/assets/images/poomsaeTestPage/ps1.png';
+import Ps2 from '/src/assets/images/poomsaeTestPage/ps2.png';
+import Ps3 from '/src/assets/images/poomsaeTestPage/ps3.png';
+import Ps4 from '/src/assets/images/poomsaeTestPage/ps4.png';
+import Ps5 from '/src/assets/images/poomsaeTestPage/ps5.png';
+import Ps6 from '/src/assets/images/poomsaeTestPage/ps6.png';
+import Ps7 from '/src/assets/images/poomsaeTestPage/ps7.png';
+import Ps8 from '/src/assets/images/poomsaeTestPage/ps8.png';
+
+const Ps_images = [Ps1, Ps2, Ps3, Ps4, Ps5, Ps6, Ps7, Ps8];
 
 const PoomsaeTestModal = ({ poomsae, onClose }) => {
     const navigate = useNavigate();
@@ -10,6 +20,8 @@ const PoomsaeTestModal = ({ poomsae, onClose }) => {
         navigate(`/ps_test/detail/${poomsae.id}`);
     };
 
+    const imageUrl  = Ps_images[poomsae.id - 1];
+
 
     return (
       <div className="modal-overlay" onClick={onClose}>
@@ -17,7 +29,7 @@ const PoomsaeTestModal = ({ poomsae, onClose }) => {
           <p className="modal-title">{poomsae.name}</p>
           <div className="modal-ps">
             <div className="modal-image-box">
-              <img src={`/src/assets/images/poomsaeTestPage/ps${poomsae.id}.png`}  alt="" className="modal-image" />
+              <img src={imageUrl}  alt="" className="modal-image" />
             </div>
             <div className="modal-desc">
               <div className="modal-ps-description">
