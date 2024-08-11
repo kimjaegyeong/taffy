82dbe61a3c12f8b7fdd2128a207db609873ac802
김재경 <dongji_11@naver.com>
2024-08-11 19:27:31 +0900

    [BE][fix] 방 종료 로직 변경

--- backend/src/main/java/com/taffy/backend/fight/controller/FightController.java
+++ backend/src/main/java/com/taffy/backend/fight/controller/FightController.java
@@ -54,8 +54,8 @@ public class FightController {
     }
 
     @DeleteMapping("/exit")
-    public ResponseEntity<ResponseDto> exitRoom(@Autowired Long memberId, @RequestParam String sessionId, @RequestParam String roomType){
-        fightService.exitRoom(memberId, sessionId, roomType);
+    public ResponseEntity<ResponseDto> exitRoom(@AuthenticationPrincipal Long memberId, @RequestParam String sessionId, @RequestParam String roomType){
+        fightService.deleteRoom(sessionId, roomType);
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto<>(HttpStatus.OK.value(), "겨루기가 정상적으로 종료되었습니다.", "success"));
     }
 }
